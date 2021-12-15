cfc1f1dca522c867ad38a09887abd0f8b4f89981
mh.khan <mh.khan@asthait.com>
2021-12-15 20:29:56 +0600

    first day end

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using WebApi_2022.Models;
+using WebApi_2022.Services;
+
+namespace WebApi_2022.Controllers
+{
+    [Route("[controller]")]
+    public class PizzasController : ControllerBase
+    {
+
+        private readonly PizzaService _pizzasService;
+        public PizzasController() 
+        {
+            _pizzasService = new PizzaService();
+        }
+
+        [HttpGet]
+        public IEnumerable<Pizza> GetAll()
+        {
+            return _pizzasService.GetAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var pizza = _pizzasService.Get(id);
+            if (pizza == null)
+            {
+                return BadRequest();
+            }
+            return Ok(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            _pizzasService.Add(pizza);
+            Console.WriteLine(pizza.Id);
+            return Ok(_pizzasService.GetAll());
+        }
+
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi_2022.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
