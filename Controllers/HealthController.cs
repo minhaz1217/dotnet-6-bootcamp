d0903bdad541a87ae2610565b1014080cccccc9a
minhazul hayat khan <minhaz1217@gmail.com>
2022-03-17 20:22:47 +0600

    added /health, /status and /exit endpoints

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_2022.Controllers
+{
+
+
+    [Route("/")]
+    public class HealthController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("ok");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok("hello world");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("exit")]
+        public IActionResult Exit()
+        {
+            this.Exit();
+            return Ok("hello world");
+        }
+
+    }
+}
